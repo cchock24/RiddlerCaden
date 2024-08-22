41cc3036873d917c4cba6947bc637a05f6a8f38e
Caden <caden.chock@menloschool.org>
2024-08-22 10:51:44 -0700

    :)

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date   |    Time    |                                                                    Update |
-|:-------|:----------:|--------------------------------------------------------------------------:|
-| Aug 22 |  8-8:30pm  | I started by thinking about my place in the universe.<br/>It was scary... |
-|        |            |                                                             And then I... |
+| Date   |    Time     |                                                                                     Update |
+|:-------|:-----------:|------------------------------------------------------------------------------:|
+| Aug 22 |  8-8:30pm   |                  I started by thinking about my place in the universe.<br/>It was scary... |
+| Aug 22 | 10:20-10:50 | I decrypted #1 :)                                               |
+|        |             |                                                                                            |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -10,9 +10,37 @@ public class Riddler {
 
     public String decryptOne(String encrypted) {
         String decrypted = "";
-
         // TODO: Complete the decryptOne() function.
+        for(int i = 0; i < encrypted.length(); i++){
+            if(encrypted.charAt(i) > 64){
+                char filler = encrypted.charAt(i);
+                int gap;
+                if(filler > 96){
+                    if(filler + 9 > 122){
+                        gap = filler + 9 - 122;
+                        filler = (char)(96 + gap);
+                    }
+                    else{
+                        filler = (char)(filler + 9);
+                    }
+                }
+                if(filler < 91){
+                    if(filler + 9 > 90){
+                        gap = filler + 9 - 90;
+                        filler = (char)(64 + gap);
+                    }
+                    else{
+                        filler = (char)(filler + 9);
+                    }
+                }
+                decrypted += filler;
+            }
+            else{
+                decrypted += encrypted.charAt(i);
+            }
 
+        }
+        System.out.println(decrypted);
         return decrypted;
     }
 
